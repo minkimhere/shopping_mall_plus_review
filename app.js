4f02c13ef6d2e27c047cee85dd3e54ce47349504
minkimhere <mj.kim.tech@gmail.com>
2022-06-07 01:07:49 +0900

    회원가입 api 생성

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const User = require("./models/user");
+
 const app = express();
 const router = express.Router();
 
@@ -12,6 +14,29 @@ const connect = () => {
 };
 
 connect();
+
+router.post("/users", async (req, res) => {
+  const { nickname, email, password, confirmPassword } = req.body;
+
+  if (password !== confirmPassword) {
+    return res.status(400).json({
+      errorMessage: "패스워드가 패스워드 확인란과 동일하지 않습니다.",
+    });
+  }
+
+  const existUser = await User.find({ $or: [{ nickname }, { email }] });
+
+  if (existUser.length) {
+    return res.status(400).json({
+      errorMessage: "이미 가입된 이메일 또는 닉네임이 있습니다.",
+    });
+  }
+
+  const user = new User({ email, nickname, password });
+  await user.save();
+
+  res.status(201).json({});
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
